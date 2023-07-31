50001547199b3a40fa889d18e7d6db1518a888b3
irasubiza1 <dpcode36@gmail.com>
2023-07-31 11:56:43 +0200

    solve current aapi

--- app/api/current/route.ts
+++ app/api/current/route.ts
@@ -1,8 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import getCurrentUser from '../../actions/getCurrentUser';
 import { NextResponse } from 'next/server';
 
-export async function GET(res: NextApiResponse) {
+export async function GET() {
 
    try {
       const currentUser = await getCurrentUser()
