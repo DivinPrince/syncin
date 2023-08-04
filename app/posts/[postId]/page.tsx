f9de7942c52c65b3938012c9fcb373f7ac122e22
irasubiza1 <dpcode36@gmail.com>
2023-08-04 12:28:55 +0200

    initial

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -3,11 +3,11 @@ import Form from "@/components/Form";
 import Post from "@/components/posts/Post";
 import PostCard from "@/components/posts/PostCard";
 import usePost from "@/hooks/usePost";
-import { useParams } from "next/navigation";
-import { FC } from "react";
-
-const PostView: FC = (props) => {
-  const postId = props.params.postId as string;
+interface Iparams {
+  postId: string;
+}
+const PostView = ({ params }: { params: Iparams }) => {
+  const postId = params.postId
 
   return (
     <div className="bg-neutral-900 rounded-[5px] h-full w-full overflow-hidden overflow-y-auto">
