61e7eedf38ca02174f1e18b7f4ccdd8e1b3705d5
irasubiza1 <dpcode36@gmail.com>
2023-08-04 14:45:46 +0200

    initial

--- components/user/UserBio.tsx
+++ components/user/UserBio.tsx
@@ -13,7 +13,7 @@ interface bioProps {
 const UserBio: React.FC<bioProps> = ({ userId }) => {
   const { data: fetchedUser, error, isLoading, mutate } = useUser(userId);
   const { data: currentUser } = useCurrentUser();
-  console.log(fetchedUser, error, isLoading);
+  console.log(fetchedUser, currentUser);
   if (isLoading) {
     return <UserBioLoader />;
   }
