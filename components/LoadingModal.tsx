1c4bf7763efbd7be5b4db220875a77b3cbd2862a
irasubiza1 <dpcode36@gmail.com>
2023-08-04 14:17:25 +0200

    initial

--- components/LoadingModal.tsx
+++ components/LoadingModal.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { BiLoaderAlt } from 'react-icons/bi';
 
 const LoadingModal = () => {
   return (
-    <div className='fixed top-0 left-0 w-full h-full bg-black text-white z-30'>loading...</div>
-  )
+    <div className="fixed top-0 left-0 w-full h-full bg-black text-white z[100] flex justify-center items-center">
+      <BiLoaderAlt className="animate-spin" size={100}/>
+    </div>
+  );
 }
 
 export default LoadingModal
