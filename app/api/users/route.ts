392523edac07e3ff2a994503f3c94825d75b290e
dp codes <124174236+Irasubiza1@users.noreply.github.com>
2023-08-04 12:45:40 +0200

    initial

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,11 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 import prisma from "@/lib/db";
 
 export async function GET(
-  req: NextApiRequest,
-  res: NextApiResponse
+  req: Request,
 ) {
   try {
     const users = await prisma.user.findMany({
