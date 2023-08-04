762c763748c3b9044de0a6947c84234b436b2944
irasubiza1 <dpcode36@gmail.com>
2023-08-04 15:11:46 +0200

    initial

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,14 +3,22 @@ import React from 'react'
 import Header from './components/Header';
 import getCurrentUser from '@/app/actions/getCurrentUser';
 import { Body } from './components/Body';
+import { signOut } from 'next-auth/react';
 
 export default async function page() {
 
    const currentUser = await getCurrentUser()
   return (
-    <div className='has-scrollbar bg-neutral-900 rounded-[5px] h-full w-full overflow-y-auto'>
-      <Header currentUser={currentUser!}/>
+    <div className="has-scrollbar bg-neutral-900 rounded-[5px] h-full w-full overflow-y-auto">
+      <Header currentUser={currentUser!} />
       <Body />
+      <button
+        onClick={() => {
+          signOut();
+        }}
+      >
+        signout
+      </button>
     </div>
-  )
+  );
 }
